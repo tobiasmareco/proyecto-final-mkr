3f27beb465dc3521e962f8808b559ca5a7ccf9ba
Ricardo Martínez <119366460+Nuwanda17@users.noreply.github.com>
2023-04-20 17:59:26 -0400

    Update tasks.routes.js

--- backend/Tasks/tasks.routes.js
+++ backend/Tasks/tasks.routes.js
@@ -1,12 +1,21 @@
 import express from 'express'
-import { createTaskController, deleteTaskController, getTaskIdController, getTasksController, updateTaskController } from '../Tasks/index.js'
-import { validatorCreateTask } from './tasks.validator.js'
+import {
+    createTaskController,
+    deleteTaskController,
+    getTaskIdController,
+    getTasksController,
+    updateTaskController
+} from '../Tasks/index.js'
+import {
+    validatorCreateTask,
+    validatorUpdateTask
+} from './tasks.validator.js'
 const tasksRoutes = express.Router()
 
 tasksRoutes.get('/', getTasksController)
 tasksRoutes.get('/:id', getTaskIdController)
 tasksRoutes.post('/', validatorCreateTask, createTaskController)
-tasksRoutes.put('/:id', updateTaskController)
+taskRoutes.put('/:id', validatorUpdateTask, updateTaskController)
 tasksRoutes.delete('/:id', deleteTaskController)
 
 
