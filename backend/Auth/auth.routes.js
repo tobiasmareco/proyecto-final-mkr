b6a6168d2cf363bf21b5006d502060017de92616
tobiasmareco <tobiasmareco2002@gmail.com>
2023-04-20 15:46:39 -0400

    completed implementation of auth

--- backend/Auth/auth.routes.js
+++ backend/Auth/auth.routes.js
@@ -7,10 +7,12 @@ import { validateForgot } from "./ForgotPassword/forgot.validate.js";
 import { forgotController } from "./ForgotPassword/forgot.controller.js";
 import { validateReset } from "./ResetPassword/reset.validator.js";
 import { resetPassword } from "./ResetPassword/reset.controller.js";
+import { confirmController } from "./confirmAccount/confirm.controller.js";
 const authRoutes = express.Router();
 
 authRoutes.post("/", validateLogin, loginController);
 authRoutes.post("/register", validateRegister, registerController);
 authRoutes.post("/forgot-password", validateForgot, forgotController);
 authRoutes.post("/reset-password/:token", validateReset, resetPassword);
+authRoutes.get("/confirm-account/:token", confirmController);
 export default authRoutes;
