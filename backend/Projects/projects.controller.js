bc14811c0fd1a4051072cc18d327e1c662887ded
tobiasmareco <tobiasmareco2002@gmail.com>
2023-04-21 12:27:01 -0400

    editing repository projects

--- backend/Projects/projects.controller.js
+++ backend/Projects/projects.controller.js
@@ -1,47 +1,75 @@
-import { createProjectService, deleteProjectService, getProjectIdService, getProjectsService, updateProjectService } from "./index.js"
+import {
+  createProjectService,
+  deleteProjectService,
+  getProjectIdService,
+  getProjectsService,
+  updateProjectService,
+} from "./index.js";
 
 export const createProjectController = async (req, res) => {
-
-    const { result, error } = await createProjectService(req.body)
-    if (error) {
-      return res.status(error.statusCode).json({ response: error.response, msg: error.msg })
-    }
-    return res.status(201).json({ response: 'success', msg: 'Proyecto creado correctamente', result })
-}
+  const { result, error } = await createProjectService(req.body);
+  if (error) {
+    return res
+      .status(error.statusCode)
+      .json({ response: error.response, msg: error.msg });
+  }
+  return res.status(201).json({
+    response: "success",
+    msg: "Proyecto creado correctamente",
+    result,
+  });
+};
 
 export const getProjectsController = async (req, res) => {
-
-  const { result, error } = await getProjectsService()
-    if (error) {
-      return res.status(error.statusCode).json({ response: error.response, msg: error.msg })
-    }
-    return res.status(201).json({ response: 'success', msg: 'Proyectos encontrados satisfactoriamente', result })
-}
-
+  const { result, error } = await getProjectsService();
+  if (error) {
+    return res
+      .status(error.statusCode)
+      .json({ response: error.response, msg: error.msg });
+  }
+  return res.status(201).json({
+    response: "success",
+    msg: "Proyectos encontrados satisfactoriamente",
+    result,
+  });
+};
 
 export const getProjectIdController = async (req, res) => {
-  
-  const { result, error } = await getProjectIdService(req.params.id)
+  const { result, error } = await getProjectIdService(req.params.id);
   if (error) {
-    return res.status(error.statusCode).json({ response: error.response, msg: error.msg })
+    return res
+      .status(error.statusCode)
+      .json({ response: error.response, msg: error.msg });
   }
-  return res.status(201).json({ response: 'success', msg: 'Proyecto  encontrado satisfactoriamente', result })
-
-}
+  return res.status(201).json({
+    response: "success",
+    msg: "Proyecto  encontrado satisfactoriamente",
+    result,
+  });
+};
 export const updateProjectController = async (req, res) => {
-
-  const { result, error } = await updateProjectService(req)
+  const { result, error } = await updateProjectService(req);
   if (error) {
-    return res.status(error.statusCode).json({ response: error.response, msg: error.msg, result })
+    return res
+      .status(error.statusCode)
+      .json({ response: error.response, msg: error.msg, result });
   }
-  return res.status(201).json({ response: 'success', msg: 'Proyecto  actualizado satisfactoriamente', result })
-
-}
+  return res.status(201).json({
+    response: "success",
+    msg: "Proyecto  actualizado satisfactoriamente",
+    result,
+  });
+};
 export const deleteProjectController = async (req, res) => {
-
-  const { result, error } = await deleteProjectService(req.params.id)
+  const { result, error } = await deleteProjectService(req.params.id);
   if (error) {
-    return res.status(error.statusCode).json({ response: error.response, msg: error.msg, result })
+    return res
+      .status(error.statusCode)
+      .json({ response: error.response, msg: error.msg, result });
   }
-  return res.status(201).json({ response: 'success', msg: 'Proyecto  eliminado satisfactoriamente', result })
-}
+  return res.status(201).json({
+    response: "success",
+    msg: "Proyecto  eliminado satisfactoriamente",
+    result,
+  });
+};
--- backend/Projects/projects.services.js
+++ backend/Projects/projects.services.js
@@ -1,48 +1,55 @@
-import { createProjectsRepository, deleteProjectRepository, getProjectIdRepository, getProjectsRepository, updateProjectRepository } from './index.js'
+import {
+  createProjectsRepository,
+  deleteProjectRepository,
+  getProjectIdRepository,
+  getProjectsRepository,
+  updateProjectRepository,
+} from "./index.js";
 
-export const createProjectService = async (project) => {
-    const response = await createProjectsRepository({ ...project })
-    if (response.response === 'error') {
-      return { error: response }
-    }
-    return { result: response.result }
-}
-
-export const getProjectsService = async (project) => {
-  const response = await getProjectsRepository({ ...project })
-  if (response.response === 'error') {
-    return { error: response }
+export const createProjectService = async (project, userId) => {
+  const response = await createProjectsRepository({ ...project }, userId);
+  if (response.response === "error") {
+    return { error: response };
   }
-  return { result: response.result }
+  return { result: response.result };
+};
 
-}
-
-
-export const getProjectIdService = async (project) => {
-
-  const response = await getProjectIdRepository({ ...project, projectId: project })
-  if (response.response === 'error') {
-    return { error: response }
+export const getProjectsService = async (project, userId) => {
+  const response = await getProjectsRepository({ ...project }, userId);
+  if (response.response === "error") {
+    return { error: response };
   }
-  return { result: response.result }
-}
+  return { result: response.result };
+};
 
-
-export const updateProjectService = async (project) => {
-
-  const response = await updateProjectRepository({ ...project, projectId: project.params.id, projectBody: project.body })
-  if (response.response === 'error') {
-    return { error: response }
+export const getProjectIdService = async (project, userId) => {
+  const response = await getProjectIdRepository(
+    { ...project, projectId: project },
+    userId
+  );
+  if (response.response === "error") {
+    return { error: response };
   }
-  return { result: response.result }
+  return { result: response.result };
+};
 
-}
-export const deleteProjectService = async (project) => {
-
-  const response = await deleteProjectRepository({ ...project, projectId: project })
-  if (response.response === 'error') {
-    return { error: response }
+export const updateProjectService = async (project, userId) => {
+  const response = await updateProjectRepository(
+    { ...project, projectId: project.params.id, projectBody: project.body },
+    userId
+  );
+  if (response.response === "error") {
+    return { error: response };
   }
-  return { result: response.result }
-
-}
+  return { result: response.result };
+};
+export const deleteProjectService = async (project, userId) => {
+  const response = await deleteProjectRepository(
+    { ...project, projectId: project },
+    userId
+  );
+  if (response.response === "error") {
+    return { error: response };
+  }
+  return { result: response.result };
+};
