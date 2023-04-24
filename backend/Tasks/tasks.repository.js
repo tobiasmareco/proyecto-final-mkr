827f796d97f237e5deb1393b1f8ce79dc138326a
tobiasmareco <tobiasmareco2002@gmail.com>
2023-04-24 12:35:54 -0400

    editing task service and repository

--- backend/Tasks/tasks.repository.js
+++ backend/Tasks/tasks.repository.js
@@ -1,182 +1,19 @@
-import Task from './tasks.model.js'
-
+import Task from "./tasks.model.js";
 
 export const taskRepository = {
   CREATE_TASK: async (task) => {
-    return await Task.create(task)
+    return await Task.create(task);
   },
   GET_TASKS: async (projectId) => {
-    return await Task.find({ projectId })
+    return await Task.find({ projectId });
   },
   GET_TASK_ID: async (taskId) => {
-    return await Task.findById(taskId)
+    return await Task.findById(taskId);
   },
-  UPDATE_TASK: async (task, projectId) => {
-
+  UPDATE_TASK: async ({ ...task }, taskId) => {
+    return await Task.findByIdAndUpdate(taskId, task);
   },
-  DELETE_TASK: async (taskId, projectId) => {
-
-  }
-}
-
-
-
-
-
-
-export const createTaskRepository = async ({
-  title,
-  description,
-  projectId,
-  status,
-  priority
-}) => {
-  try {
-    //Crea un nuevo objeto de tarea
-    const newTask = {
-      title,
-      description,
-      projectId,
-      status,
-      priority
-    }
-    //Crea la tarea en la base de datos
-    const createTask = await Task.create(newTask)
-    //Retorna un objeto con la respuesta y el resultado de la tarea creada
-    return {
-      response: 'success',
-      result: createTask
-    }
-  } catch (error) {
-    //En caso de error, retorna un objeto con la respuesta de error y el error específico
-    return {
-      response: 'error',
-      error,
-      statusCode: 500
-    }
-  }
-}
-
-
-
-export const getTasksRepository = async (projectId) => {
-  try {
-    // Obtener todas las tareas de un proyecto específico
-    const tasks = await Task.find({
-      projectId
-    }).sort({
-      updatedAt: -1
-    })
-
-    // Si obtuvo resultados en el array
-    if (tasks.length > 0) {
-      return {
-        response: 'success',
-        result: tasks
-      }
-    }
-
-    // No obtuvo resultados
-    return {
-      response: 'error',
-      statusCode: 404,
-      msg: new Error('No se encontraron tareas para el proyecto especificado.').message
-    }
-  } catch (error) {
-    return {
-      response: 'error',
-      error,
-      statusCode: 500
-    }
-  }
-}
-export const getTaskIdRepository = async ({
-  taskId
-}) => {
-  try {
-    //Obtener tarea por ID
-    const task = await Task.findById(taskId);
-    //Si la tarea existe
-    if (task) {
-      return {
-        response: 'success',
-        result: task
-      };
-    }
-    //No se encontró tarea
-    return {
-      response: 'error',
-      statusCode: 404,
-      msg: new Error('No se encontró la tarea.').message
-    };
-  } catch (error) {
-    return {
-      response: 'error',
-      error,
-      statusCode: 500
-    };
-  }
-}
-export const updateTaskRepository = async (taskId, taskBody) => {
-  try {
-    // Buscar la tarea por su ID
-    let task = await Task.findById(taskId);
-    // Si no se encuentra la tarea, devolver error 404
-    if (!task) {
-      return {
-        response: 'error',
-        statusCode: 404,
-        msg: new Error('No se encontró la tarea.').message
-      };
-    }
-    // Actualizar los campos de la tarea
-    task.title = taskBody.title || task.title;
-    task.description = taskBody.description || task.description;
-    task.status = taskBody.status || task.status;
-    task.priority = taskBody.priority || task.priority;
-    // Guardar los cambios en la base de datos
-    task = await task.save();
-    // Devolver la tarea actualizada
-    return {
-      response: 'success',
-      result: task
-    };
-  } catch (error) {
-    // Si ocurre un error, devolverlo
-    return {
-      response: 'error',
-      error,
-      statusCode: 500
-    };
-  }
-};
-
-export const deleteTaskRepository = async (taskId) => {
-  try {
-    // Buscar la tarea por su ID
-    let task = await Task.findById(taskId);
-    // Si no se encuentra la tarea, devolver error 404
-    if (!task) {
-      return {
-        response: 'error',
-        statusCode: 404,
-        msg: new Error('No se encontró la tarea.').message
-      };
-    }
-    // Eliminar la tarea de la base de datos
-    await Task.deleteOne({
-      _id: taskId
-    });
-    // Devolver la tarea eliminada
-    return {
-      response: 'success'
-    };
-  } catch (error) {
-    // Si ocurre un error, devolverlo
-    return {
-      response: 'error',
-      error,
-      statusCode: 500
-    };
-  }
+  DELETE_TASK: async (taskId) => {
+    return await Task.findByIdAndDelete(taskId);
+  },
 };
--- backend/Tasks/tasks.services.js
+++ backend/Tasks/tasks.services.js
@@ -1,84 +1,103 @@
-import Project from '../Projects/projects.model.js';
-import User from '../Users/users.model.js';
-import { returnError } from '../helpers/returnError.js';
-import {
-    createTaskRepository,
-    deleteTaskRepository,
-    getTaskIdRepository,
-    getTasksRepository,
-    taskRepository,
-    updateTaskRepository
-} from './index.js'
-import Task from './tasks.model.js';
+import Project from "../Projects/projects.model.js";
+import User from "../Users/users.model.js";
+import { returnError } from "../helpers/returnError.js";
+import { taskRepository } from "./index.js";
+import Task from "./tasks.model.js";
 
 export const createTaskService = async (newTask, userId) => {
-    try {
-        console.log(userId)
-        const { premium } = await User.findById(userId)
-        const tasks = await Task.find({ projectId: newTask.projectId }) || [];
-        if (!premium && tasks.length >= 4) {
-            return { error: returnError(404, 'El usuario no posee cuenta premium, solo es posible tener 4 tareas.') }
-        }
-        const task = await taskRepository.CREATE_TASK(newTask);
-        return { result: task }
-    } catch (error) {
-        return { error: returnError(403, error.message) }
+  try {
+    console.log(userId);
+    const { premium } = await User.findById(userId);
+    const projectExist = await Project.findById(newTask.projectId);
+    if (!projectExist) {
+      return {
+        error: returnError(
+          404,
+          `No existe el proyecto con id ${newTask.projectId}.`
+        ),
+      };
     }
-}
-
+    const tasks = (await Task.find({ projectId: newTask.projectId })) || [];
+    if (!premium && tasks.length >= 4) {
+      return {
+        error: returnError(
+          404,
+          "El usuario no posee cuenta premium, solo es posible tener 4 tareas."
+        ),
+      };
+    }
+    const task = await taskRepository.CREATE_TASK(newTask);
+    return { result: task };
+  } catch (error) {
+    return { error: returnError(403, error.message) };
+  }
+};
 
 export const getTasksService = async (projectId) => {
-    try {
-
-        const tasks = await taskRepository.GET_TASKS(projectId);
-        if (!tasks) {
-            return { error: returnError(404, "El Proyecto no tiene tareas.") }
-        }
-        return { result: tasks }
-    } catch (error) {
-        return { error: returnError(403, error.message) }
+  try {
+    const projectExist = await Project.findById(projectId);
+    if (!projectExist) {
+      return {
+        error: returnError(404, `No existe el proyecto con id ${projectId}.`),
+      };
     }
-}
+    const tasks = await taskRepository.GET_TASKS(projectId);
+    if (!tasks || tasks.length < 1) {
+      return { error: returnError(404, "El Proyecto no tiene tareas.") };
+    }
+    return { result: tasks };
+  } catch (error) {
+    return { error: returnError(403, error.message) };
+  }
+};
 export const getTaskIdService = async (taskId) => {
-    console.log('entre', taskId)
-    try {
-        const task = await taskRepository.GET_TASK_ID(taskId);
-        if (!task) {
-            return { error: returnError(404, `No se ha encontrado la tarea con id ${taskId}`) }
-        }
-        return { result: task }
-    } catch (error) {
-        return { error: returnError(403, error.message) }
+  console.log("entre", taskId);
+  try {
+    const task = await taskRepository.GET_TASK_ID(taskId);
+    if (!task) {
+      return {
+        error: returnError(
+          404,
+          `No se ha encontrado la tarea con id ${taskId}`
+        ),
+      };
     }
-}
+    return { result: task };
+  } catch (error) {
+    return { error: returnError(403, error.message) };
+  }
+};
 
-export const updateTaskService = async (task) => {
-    // llamamos a la función updateTaskRepository
-    const response = await updateTaskRepository({
-        ...task,
-        taskId: task.params.id,
-        taskBody: task.body
-    })
-    // verificamos si la respuesta de la base de datos es un error, en caso afirmativo devolvemos un objeto con la información del error
-    if (response.response === 'error') {
-        return {
-            error: response
-        }
+export const updateTaskService = async (newTask, taskId) => {
+  try {
+    const task = await Task.findById(taskId);
+    console.log(task);
+    if (!task) {
+      return { error: returnError(404, `No existe tarea con el id ${taskId}`) };
     }
-    // si no hay error devolvemos un objeto con la información actualizada de la tarea
-    return {
-        result: response.result
-    }
-}
+    task.title = newTask.title || task.title;
+    task.description = newTask.description || task.description;
+    task.status = newTask.status || task.status;
+    task.priority = newTask.priority || task.priority;
+    const updatedTask = await taskRepository.UPDATE_TASK(task, taskId);
+    return { result: task };
+  } catch (error) {
+    console.log(error);
+    return { error: returnError(403, error.message) };
+  }
+};
 
 export const deleteTaskService = async (taskId) => {
-    const response = await deleteTaskRepository(taskId) //Llamamos a la función deleteTaskRepository
-    if (response.response === 'error') { // Si la respuesta indica un error, devuelve un objeto con un error
-        return {
-            error: response
-        }
+  try {
+    const task = await Task.findById(taskId);
+    console.log(task);
+    if (!task) {
+      return { error: returnError(404, `No existe tarea con el id ${taskId}`) };
     }
-    return {
-        result: response.result
-    } // Si la respuesta es exitosa, devuelve el resultado de la operación
-}
+    const deletedTask = await taskRepository.DELETE_TASK(taskId);
+    return { result: deletedTask };
+  } catch (error) {
+    console.log(error);
+    return { error: returnError(403, error.message) };
+  }
+};
