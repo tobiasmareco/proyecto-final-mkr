0d981f7a604a2c188140fe02e6921b264fbe867e
tobiasmareco <tobiasmareco2002@gmail.com>
2023-04-27 21:09:34 -0400

    editing frontend auth

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import { toast } from 'react-toastify'
-function AlertMsg({ message, typeMsg }) {
-  return typeMsg == 'error' ? (toast.error(message)) : (toast.success(message))
+import React from "react";
+import { toast } from "react-toastify";
+function AlertMsg({ alert }) {
+  // return toast.error(alert.msg) || "errror";
+  return (
+    <div
+    // // className={`${
+    // //   alert.error ? "bg-red-500/80 text-yellow-50" : "bg-sky-800 text-white"
+    // // } max-w-md mx-auto mb-4 px-4 py-2 rounded-md shadow-sm border-black font-bold text-center`}
+    >
+      {/* {alert.msg} */}
+      {toast.error(alert.msg) || "errror"} // imprime varias veces
+    </div>
+  );
 }
 
-export default AlertMsg
+export default AlertMsg;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { ToastContainer } from 'react-toastify'
-import '../node_modules/react-toastify/dist/ReactToastify.css'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "../node_modules/react-toastify/dist/ReactToastify.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <>
     <App />
     <ToastContainer />
-  </React.StrictMode>,
-)
+  </>
+  // </React.StrictMode>,
+);
